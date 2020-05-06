2a62652d203ee524630472b41a1181cf3527c7be
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-06 16:36:08 -0700

    adding constructor! to chat swing for layout!

--- code/Client/chatSwing.java
+++ code/Client/chatSwing.java
@@ -4,36 +4,64 @@ import java.awt.event.*;
 import java.awt.Color;
 
 
-class ChatSwing implements ActionListener
+class ChatSwing
 {
+
+    
+    JFrame frame;
+    JLabel label;
+    JTextArea chatbubble;
+    JTextArea rooms;
+    JTextField textbox1;
+    JButton button; 
+
 
 /**
  * very rough start to the layout! 
  * @param not used
  */
-    private static void createAndDisplayGUI()
+    ChatSwing()
     {
+
+
+        /*JFrame frame;
+        JLabel label;
+        JTextArea chatbubble;
+        JTextArea rooms;
+        JTextField textbox1;
+        JButton button; */
+        
+        
         //the screen 
-        JFrame frame= new JFrame("IRC Chat");
+        frame= new JFrame("IRC Chat");
         frame.setBackground(Color.LIGHT_GRAY);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //label for window
-        JLabel label = new JLabel("Welcome to chat");
+        label = new JLabel("Welcome to chat");
         label.setBounds(50, 100, 400, 100);
         label.setLocation(0, 0);
         frame.add(label);
-       
-        JTextArea chatbubble = new JTextArea("messages shown here");
+      
+        
+        //This is for the actual chat text 
+        chatbubble = new JTextArea("messages shown here");
         chatbubble.setBounds(50,300,200,200);
         chatbubble.setBackground(Color.lightGray);
         frame.add(chatbubble);
 
 
+        //this is for the Rooms 
+        rooms= new JTextArea("Rooms");
+        rooms.setBounds(350, 50, 100, 400);
+        frame.add(rooms);
+
+
+
 
 
         //Adding a textbox for the chatting 
-        JTextField textbox1= new JTextField("Enter Message Here");
+        textbox1= new JTextField("Enter Message Here");
         textbox1.setBounds(50,500,400,50);
         textbox1.setBackground(Color.CYAN);
         frame.add(textbox1);
@@ -42,10 +70,24 @@ class ChatSwing implements ActionListener
         
         //assuming we have an open button
         //KATIE! FIX THIS BUTTON PLACEMENT 
-        JButton button= new JButton("Click to Send Message");
-        button.setBounds(50, 600, 95, 30);
+        button= new JButton("Click to Send Message");
+        button.setBounds(100, 600, 95, 30);
+       // frame.add(button, BorderLayout.LINE_START);
         frame.add(button);
+        
+        /**
+         * not used not sure how to get this to work yet! 
+         * @param 
+         */
+        button.addActionListener( new ActionListener(){
+            
+        public void actionPerformed(ActionEvent e){
+            addMessageToChatBubble("hello");
 
+              
+         }
+
+        });
 
         //frame controls
         frame.pack();
@@ -57,16 +99,16 @@ class ChatSwing implements ActionListener
     /**
      * This will be triggered by button to send message! and show up on chat
      * bubble 
-     * @param ActionEvent 
+     * @param string being sent in to be added to chat 
      */
-    public void actionPerformed(ActionEvent e){
-
+    public static void addMessageToChatBubble(String event ){
     }
+
     public static void main(String [] args)
     {
         javax.swing.SwingUtilities.invokeLater(new Runnable(){
         public void run(){
-            createAndDisplayGUI();
+            new ChatSwing();
         }
     });
  
