3ef3ed7a8e5c49aa99e8fe1bb118f5c8de81acee
Joseph Jesse <jjesse@pdx.edu>
2020-05-05 21:12:01 -0700

    Added a server (still working on it)

--- code/Server/Server.java
+++ code/Server/Server.java
@@ -1,7 +1,47 @@
 package code.Server;
 
-public class Server {
-    public static void main(String [] notUsed) {
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+import code.IRC_Packets.IRC_Packet;
 
+/**
+ * Server class for the server side of the IRC
+ */
+public class Server {
+
+    // Class fields
+    private static final int port = 5000; // Port number for the server process
+    private ServerSocket welcomeSocket;
+
+    /**
+     * Main program that runs the IRC server
+     * 
+     * @param notUsed
+     */
+    public static void main(String[] notUsed) throws Exception {
+        Server server = new Server();
+
+        while (true) {
+            Socket newConnection = server.welcomeSocket.accept();
+
+            ObjectInputStream inFromClient = new ObjectInputStream(newConnection.getInputStream());
+            ObjectOutputStream outToClient = new ObjectOutputStream(newConnection.getOutputStream());
+
+            IRC_Packet clientPacket = (IRC_Packet) inFromClient.readObject();
+        }
+    }
+
+
+
+    //Class methods
+
+    /**
+     * Private constructor to keep from outside use.
+     * 
+     * @throws IOException
+     */
+    private Server() throws IOException {
+        this.welcomeSocket = new ServerSocket(port);
     }
 }
