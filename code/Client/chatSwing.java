0f43b52bf7612b1fd6bdb98838de72d43bf37916
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-07 12:26:45 -0700

    Fixing layout of IRC Swing Chat

--- code/Client/chatSwing.java
+++ code/Client/chatSwing.java
@@ -14,12 +14,14 @@ class ChatSwing
     JFrame frame;
     JLabel label;
     JTextArea chatbubble;
-    //JTextArea rooms;
     JTextField textbox1;
     JButton button; 
     JComboBox roomMenu;
     JLabel labelRoom;
     String rooms[]= {"General", "Private Chat-Joseph Jesse", "Private Chat -Katie Rosas", "Games"};
+    JLabel allRoomsList;
+    JComboBox allRoomMenu;
+    String allRooms[]={"Barney", "is", "a ", "dinosaur", "who", "lives"};
 
 
 /**
@@ -28,15 +30,16 @@ class ChatSwing
  */
     ChatSwing()
     {
+        Color bgColor = new Color(47,79,79);
         //the screen 
         frame= new JFrame("IRC Chat");
-        frame.setBackground(Color.LIGHT_GRAY);
+        frame.getContentPane().setBackground(bgColor);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
        
         frame.setLayout(new GridBagLayout()); // no need to use a layout manager
         GridBagConstraints c= new GridBagConstraints();
-        c.insets= new Insets(10,10,10,10);
+        c.insets= new Insets(2,0,0,0);
 
 
 
@@ -46,47 +49,50 @@ class ChatSwing
         c.ipadx=40;
         c.ipady=20;
         label = new JLabel("Welcome to chat");
-  //      label.setBounds(50, 100, 400, 100);
-        label.setLocation(0, 0);
+        label.setForeground(Color.DARK_GRAY);
+
         frame.add(label,c);
       
         
         //This is for the actual chat text 
         c.gridx=0;
-        c.gridy=1;
-        c.ipadx=90;
-        c.ipady=300;
+        c.gridy=2;
+        c.ipadx=200;
+        c.ipady=400;
         chatbubble = new JTextArea("messages shown here");
-//        chatbubble.setBounds(50,300,200,200);
         chatbubble.setBackground(Color.lightGray);
         frame.add(chatbubble,c);
 
 
-        //this is for the Rooms 
-        //change this to a menu screen!! or checkboxes :?
-        //so we can tell what room the person is in 
-        /*
-        rooms= new JTextArea("Rooms");
-        rooms.setBounds(350, 50, 100, 400);
-        frame.add(rooms);
-        */
-
-        c.gridx=1;
-        c.gridy=0;
-       // c.ipadx=30;
-        c.ipady=90;
-        labelRoom = new JLabel("Rooms Available");
-//        labelRoom.setBounds(350,10,400, 100);
-        labelRoom.setBackground(Color.GREEN);
-        frame.add(labelRoom,c);
 
         c.gridx=1;
         c.gridy=1;
-        //c.ipadx=20;
-        c.ipady=300;
+        c.ipadx=0;
+        c.ipady=90;
+        labelRoom = new JLabel("Rooms Subscribed");
+        labelRoom.setBackground(Color.GREEN);
+        frame.add(labelRoom,c);
+
+        c.gridx=2;
+        c.gridy=1;
+        c.ipadx=10;
+        c.ipady=10;
         roomMenu= new JComboBox<>(rooms);
-//        roomMenu.setBounds(350, 50, 100, 100);
-        frame.add(roomMenu,c);
+        frame.add(roomMenu, c);
+
+         c.gridx=1;
+         c.gridy=0;
+         c.ipadx=10;
+         c.ipady=0;
+         allRoomsList= new JLabel ("All Rooms Available");
+         frame.add(allRoomsList, c);
+
+         c.gridx=2;
+         c.gridy=0;
+         c.ipadx=10;
+         c.ipady=0;
+         allRoomMenu= new JComboBox <> (allRooms);
+         frame.add(allRoomMenu, c);
 
 
 
@@ -95,24 +101,20 @@ class ChatSwing
 
         //Adding a textbox for the chatting 
         c.gridx=0;
-        c.gridy=3;
+        c.gridy=4;
         c.ipadx=90;
         c.ipady=20;
         textbox1= new JTextField("Enter Message Here");
-        //textbox1.setBounds(50,500,400,50);
         textbox1.setBackground(Color.CYAN);
         frame.add(textbox1,c);
 
         
         
-        //assuming we have an open button
-        //KATIE! FIX THIS BUTTON PLACEMENT 
         c.gridx=1;
-        c.gridy=3;
-        c.ipadx=20;
-        c.ipady=20;
+        c.gridy=4;
+        c.ipadx=10;
+        c.ipady=10;
         button= new JButton("Click to Send Message");
-        //button.setBounds(100, 600, 95, 30);
         frame.add(button,c);
         
         /**
@@ -133,7 +135,7 @@ class ChatSwing
         frame.pack();
 
         frame.setLayout(null);
-        frame.setSize(500,600);
+        frame.setSize(900,900);
         frame.setVisible(true);
 
     }  
