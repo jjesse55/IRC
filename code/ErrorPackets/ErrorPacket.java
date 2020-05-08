013311ffbe525f5bbdfc4f57c8a026305fa6ee27
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-07 22:49:08 -0700

    hierachy changes

--- code/ErrorPackets/ErrorPacket.java
+++ code/ErrorPackets/ErrorPacket.java
@@ -1,5 +1,14 @@
 package code.ErrorPackets;
+import code.Codes.OpCodes;
+import code.IRC_Packets.IRC_Packet;
 
-public class ErrorPacket {
+public class ErrorPacket extends IRC_Packet{
     
+   
+    public ErrorPacket(OpCodes opCode){
+        super(opCode);
+
+    }
 }
+
+
--- code/OpPackets/OpPackets.java
+++ code/OpPackets/OpPackets.java
@@ -1,5 +1,10 @@
 package code.OpPackets;
+import code.Codes.OpCodes;
+import code.IRC_Packets.IRC_Packet;
 
-public class OpPackets {
-    
+public class OpPackets extends IRC_Packet{
+    public OpPackets(OpCodes opCode)
+    {
+        super(opCode);
+    }
 }
