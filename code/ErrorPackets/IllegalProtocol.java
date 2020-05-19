163a0725ff80de0e00c47607d9fbb931245ca976
Joseph Jesse <jjesse@pdx.edu>
2020-05-19 16:49:31 -0700

    fixing syntax err

--- code/ErrorPackets/IllegalProtocol.java
+++ code/ErrorPackets/IllegalProtocol.java
@@ -6,6 +6,6 @@ import code.Codes.OpCodes;
 public class IllegalProtocol extends ErrorPacket {
 
     public IllegalProtocol() {
-        super(OpCodes.OP_CODE_ERR, ErrorCodes.IRC_ERR_ILLEGAL_PROTOCOL)
+        super(OpCodes.OP_CODE_ERR, ErrorCodes.IRC_ERR_ILLEGAL_PROTOCOL);
     }
 }
