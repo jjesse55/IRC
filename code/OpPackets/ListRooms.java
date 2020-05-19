5d6d2406c9ab41983121bed53f4d6e9b27a944a7
Joseph Jesse <jjesse@pdx.edu>
2020-05-19 16:51:05 -0700

    comments

--- code/OpPackets/ListRooms.java
+++ code/OpPackets/ListRooms.java
@@ -2,6 +2,10 @@ package code.OpPackets;
 
 import code.Codes.OpCodes;
 
+
+/**
+ * IRC packet send to server requesting to get a list of all current rooms
+ */
 public class ListRooms extends OpPackets{
    
     /**
@@ -11,6 +15,4 @@ public class ListRooms extends OpPackets{
     public ListRooms(){
         super(OpCodes.OP_CODE_LIST_ROOMS);
     }
-
-    
 }
