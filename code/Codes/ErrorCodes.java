b0174161723425dc69b6ce6074db7b072d9f2007
Joseph Jesse <jjesse@pdx.edu>
2020-05-19 15:51:34 -0700

    added err code for illegal protocol

--- code/Codes/ErrorCodes.java
+++ code/Codes/ErrorCodes.java
@@ -7,7 +7,8 @@ public enum ErrorCodes {
     IRC_ERR_UNKNOWN(0x00000001),            //Unknown error (general error)
     IRC_ERR_ILLEGAL_OPCODE(0x00000002),     //Illegal OpCode specified
     IRC_ERR_ILLEGAL_LENGTH(0x00000003),     //Wrong version
-    IRC_ERR_NAME_EXISTS(0x00000004);        //Name for a client is pre-existant
+    IRC_ERR_NAME_EXISTS(0x00000004),        //Name for a client is pre-existant
+    IRC_ERR_ILLEGAL_PROTOCOL(0x00000005);   //Wrong protocol specified to communicate with the server
 
     private int errCode;    //Integer value representing the errCode
 
