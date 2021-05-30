64edb8d70bee566415ff815fd484cb01594379b9
Taishawn Fontleroy <taishawn@pdx.edu>
2021-05-30 14:57:25 -0700

    Comment deletion

--- code/Codes/ErrorCodes.java
+++ code/Codes/ErrorCodes.java
@@ -6,7 +6,7 @@ package code.Codes;
 public enum ErrorCodes {
     IRC_ERROR_UNKNOWN,            
     IRC_ERROR_ILLEGAL_OPCODE,    
-    IRC_ERROR_ILLEGAL_LENGTH,     //Wrong version
+    IRC_ERROR_ILLEGAL_LENGTH,
     IRC_ERROR_NAME_EXISTS,       
     IRC_ERROR_ILLEGAL_PROTOCOL,  
     IRC_ERROR_INVALID_ROOM_NAME; 
--- code/Codes/OpCodes.java
+++ code/Codes/OpCodes.java
@@ -4,9 +4,9 @@ package code.Codes;
  * OpCodes sent with IrcPacket objects
  */
 public enum OpCodes {
-    OP_CODE_ERROR,                        //An error in the request from the client
-    OP_CODE_KEEP_ALIVE,                 //Keep the connection alive between client/server
-    OP_CODE_HELLO,                      //Handshake
+    OP_CODE_ERROR,                       
+    OP_CODE_KEEP_ALIVE,                 
+    OP_CODE_HELLO,                    
     OP_CODE_LIST_ROOMS,                 
     OP_CODE_LIST_ROOMS_RESPONSE,       
     OP_CODE_LIST_USERS,                 
@@ -16,7 +16,7 @@ public enum OpCodes {
     OP_CODE_LEAVE_ROOM,              
     OP_CODE_LEAVE_ROOM_response,           
     OP_CODE_SEND_MESSAGE,               
-    OP_CODE_SEND_MESSAGE_RESPONSE,      //Server sending a message to all users in a room
+    OP_CODE_SEND_MESSAGE_RESPONSE, 
     OP_CODE_SEND_PRIVATE_MESSAGE,       
     OP_CODE_GOODBYE;                    
 }
